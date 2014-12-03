e73cb4f2d4c610aeb60ec6650f40539836e06e9a
Lou Oriondo <loriondo@uga.edu>
2014-12-02 22:18:02 -0500

    Begin 99 Problems in C#.
    First 6 problems completed from Lists subset of the 99 problems found
    here: http://aperiodic.net/phil/scala/s-99/.
    
    More solutions to come as finals pass and christmas break starts.

---
+++ 99 C# Problems/Main.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace NinetyNineProblemsC
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Providing solutions for problem set.");
+
+			Lists listSolutions = new Lists ();
+			listSolutions.runSolutions ();
+
+		}
+	}
+}
